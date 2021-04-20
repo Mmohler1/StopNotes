5c1e704031f7ae53dfc3231557743c61baebc6b1
Mmohler1 <71154838+Mmohler1@users.noreply.github.com>
2021-04-20 00:59:37 -0700

    Add files via upload

---
+++ StopNotes/StopNotes/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace StopNotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.mainTimerLabel = new System.Windows.Forms.Label();
+            this.buttonNote = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownMinutes = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(183, 226);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(111, 53);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(479, 226);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(111, 53);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // mainTimerLabel
+            // 
+            this.mainTimerLabel.AutoSize = true;
+            this.mainTimerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.mainTimerLabel.Location = new System.Drawing.Point(386, 104);
+            this.mainTimerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.mainTimerLabel.Name = "mainTimerLabel";
+            this.mainTimerLabel.Size = new System.Drawing.Size(221, 58);
+            this.mainTimerLabel.TabIndex = 3;
+            this.mainTimerLabel.Text = "00:00:00";
+            // 
+            // buttonNote
+            // 
+            this.buttonNote.Location = new System.Drawing.Point(324, 226);
+            this.buttonNote.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.buttonNote.Name = "buttonNote";
+            this.buttonNote.Size = new System.Drawing.Size(111, 53);
+            this.buttonNote.TabIndex = 1;
+            this.buttonNote.Text = "Timestamp (Press ?)";
+            this.buttonNote.UseVisualStyleBackColor = true;
+            this.buttonNote.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(243, 314);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(110, 53);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save To Text File";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 361);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(406, 314);
+            this.buttonReset.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(111, 53);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(433, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 39);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Timer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(268, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Minute(s) and";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 124);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Second(s)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(194, 20);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Start the timer at:";
+            // 
+            // numericUpDownMinutes
+            // 
+            this.numericUpDownMinutes.Location = new System.Drawing.Point(199, 77);
+            this.numericUpDownMinutes.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDownMinutes.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDownMinutes.Name = "numericUpDownMinutes";
+            this.numericUpDownMinutes.Size = new System.Drawing.Size(61, 26);
+            this.numericUpDownMinutes.TabIndex = 13;
+            // 
+            // numericUpDownSeconds
+            // 
+            this.numericUpDownSeconds.Location = new System.Drawing.Point(199, 122);
+            this.numericUpDownSeconds.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDownSeconds.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDownSeconds.Name = "numericUpDownSeconds";
+            this.numericUpDownSeconds.Size = new System.Drawing.Size(61, 26);
+            this.numericUpDownSeconds.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 407);
+            this.Controls.Add(this.numericUpDownSeconds);
+            this.Controls.Add(this.numericUpDownMinutes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonNote);
+            this.Controls.Add(this.mainTimerLabel);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "Form1";
+            this.Text = "StopwatchNotes";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label mainTimerLabel;
+        private System.Windows.Forms.Button buttonNote;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinutes;
+        private System.Windows.Forms.NumericUpDown numericUpDownSeconds;
+    }
+}
+
---
+++ StopNotes/StopNotes/Form1.cs
@@ -0,0 +1,213 @@
+﻿/*Author: MikeJRthe2nd
+Date: 1/9/2020
+Program: A stopwatch program that saves the final time the stopwatch ends at to a text file. 
+        Also have it so that when a key is pressed it saves the time when that key was pressed and puts that on the text file too.
+
+        Note: Was called StopNotes, but another program has that name so I switched it to StopwatchNotes. Doesn't roll
+        off the tounge as well, but it is easier to understand. 
+
+        Things to add:
+
+        -The extra timer only goes to anything below 24 hours, but can handle higher ammounts if needed.
+
+
+        -Set timestamp to a key of the users choice. Since this needs to be used as a globel key it currently only
+            works with the ? key. Might add ability to change hotkey later. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Stopwatch
+using System.Diagnostics;
+using System.Threading;
+
+using System.Runtime.InteropServices; //Windows API
+//File
+using System.IO;
+
+namespace StopNotes
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopNotes = new Stopwatch();
+        String noteTime = "";
+        long extra; //For adding extra time
+        bool resetClick = false;
+
+        private IntPtr thisForm;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(String theClassName, String theAppName);
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id );
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            thisForm = FindWindow(null, "StopwatchNotes");
+
+            RegisterHotKey(thisForm, 1, (uint) 0, (uint) Keys.OemQuestion);
+
+        }
+
+
+        //Starts the timer
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            stopNotes.Start(); //Starts timer
+            timer1.Start();
+
+            //Turns off these buttons when the timer is going
+            buttonReset.Enabled = false;
+            buttonSave.Enabled = false;
+            textBox1.Enabled = false;
+            numericUpDownMinutes.Enabled = false;
+            numericUpDownSeconds.Enabled = false;
+
+            //Sets extra to be the converted number of ticks per second and min.
+            extra = ((long)numericUpDownMinutes.Value * 600000000) +    //1 minute is 600 million ticks
+                    ((long)numericUpDownSeconds.Value * 10000000);        //1 second is 10 million ticks
+
+        }
+
+        //Stops the Timer
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Stops the timers
+            stopNotes.Stop(); 
+            timer1.Stop();
+
+            //Turns on these buttons when the timer has stopped 
+            buttonReset.Enabled = true;
+            buttonSave.Enabled = true;
+            textBox1.Enabled = true;
+            numericUpDownMinutes.Enabled = true;
+            numericUpDownSeconds.Enabled = true;
+
+
+            //I think this needs to be added to the textbox Does nothing as of now.
+            TimeSpan ts = (stopNotes.Elapsed + new TimeSpan(extra));
+            String totalTime = ts.Hours.ToString().PadLeft(2,'0') + ":" + 
+                ts.Minutes.ToString().PadLeft(2, '0') + ":" + 
+                ts.Seconds.ToString().PadLeft(2, '0'); //Formats time
+
+        }
+
+        //Timestamps the current time into a string or text file
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            TimeSpan ts2 = (stopNotes.Elapsed + new TimeSpan(extra));
+            noteTime = noteTime + "\n~" + ts2.Hours.ToString().PadLeft(2, '0') + ":" +
+                ts2.Minutes.ToString().PadLeft(2, '0') + ":" +
+                ts2.Seconds.ToString().PadLeft(2, '0'); //Formats time
+
+            textBox1.Text = noteTime; //saves time to textbox
+
+            //Scrolls to bottom of textbox
+            textBox1.SelectionStart = textBox1.TextLength;
+            textBox1.ScrollToCaret();
+            
+        }
+
+        //Save to text file. Copied most of this.
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream save = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter saveW = new StreamWriter(save))
+                {
+                    saveW.Write(textBox1.Text);
+                }
+            }
+        }
+
+        //Textbox will save what's put into it.
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //If reset was clicked then don't add the spaces
+            if (resetClick == true)
+            {
+                resetClick = false;
+            }
+            else
+            {
+                noteTime = textBox1.Text + "\r\n";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            TimeSpan ts3 = (stopNotes.Elapsed + new TimeSpan(extra));
+
+            //Timer displays
+            mainTimerLabel.Text = ts3.Hours.ToString().PadLeft(2, '0') + ":" +
+                ts3.Minutes.ToString().PadLeft(2, '0') + ":" +
+                ts3.Seconds.ToString().PadLeft(2, '0'); //Formats time
+        }
+
+        //Resets the timer and clears the timestamps.
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure? This will delete your timestamps.", "Confirm Reset",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                stopNotes.Reset();
+                timer1.Dispose();
+                noteTime = "";
+                mainTimerLabel.Text = "00:00:00";
+                resetClick = true;
+                textBox1.Text = noteTime; //saves time to textbox
+            }
+        }
+
+        //Unregisters the Hotkey when the window is closed
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            UnregisterHotKey(thisForm, 1);
+        }
+
+        //Tells the program what to do when a hotkey is pressed
+        protected override void WndProc(ref Message keyPressed)
+        {
+            if(keyPressed.Msg == 0x0312 )
+            {
+                buttonNote.PerformClick(); //Click the timestamp button
+            }
+            base.WndProc(ref keyPressed);
+        }
+
+
+        //Hotkey Asignment
+        public enum fsModifiers
+        {
+            Alt = 0x0001,
+            Control = 0x0002,
+            Shift = 0x0004,
+            Window = 0x0008,
+        }
+    }
+}
